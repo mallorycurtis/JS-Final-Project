a5c62d3a0aa2f263e308450db61ae9c784ffe408
mallory.curtis92 <mallory.curtis92@yahoo.com>
2015-08-23 23:38:44 -0400

    Work in Progress

---
+++ action_page.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Thank you for your question!</title>
+	</head>
+	<body>
+		<h1>
+			 Thank you for your submission!
+		</h1>
+		<p>
+			I will return your email shortly. <br>
+			Madison
+		</p>
+	</body>
+</html>
