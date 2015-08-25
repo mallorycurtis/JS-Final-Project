b9ad3696e48aff2c41f53344833cc6b741e2759d
mallory.curtis92 <mallory.curtis92@yahoo.com>
2015-08-25 19:12:17 -0400

    Theme, CSS changed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-
+$(document).on('ready', function(){
+	$("#order-form").submit(function() {
+		var abort = false; 
+		$("div.error").remove(); 
+		$(':input[required]').each(function(){
+			if ($(this).val()==='') { 
+				abort = true; 
+			}
+		});
+		
+		if (abort) {return false;} else {return true;} //
+		
+        });
+	$('#order-form').validate({
+		"rules" : {
+			"#your-name": {
+			required: true,
+			digits: false,
+			maxlength: 128
+			},
+			"#your-email": {
+			required: true,
+			},
+			"#your-phone": {
+			required: false,
+			},
+			"#your-input": {
+			required: true,
+			maxlength: 500,
+			},
+		}
+	});
+});
